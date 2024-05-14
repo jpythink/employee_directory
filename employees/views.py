69082afb65ef10795bc0d3ccf629c1a9f3f70714
jpythink <jaynilrbpatel@gmail.com>
2024-05-14 13:11:46 +0530

    display employee list

---
+++ employees/views.py
@@ -0,0 +1,15 @@
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Employee
+
+# def employee_details(request, pk):
+#     try:
+#         employee = Employee.objects.get(pk=pk)
+#         print(employee)
+#     except:
+#         raise Http404
+
+def employee_details(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    print(employee)
+    return HttpResponse(employee)
